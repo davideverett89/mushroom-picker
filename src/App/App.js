c54f5cb2859279e9ba13c63d57bb98798f57d8aa
David Everett <davideverett1989@gmail.com>
2020-05-13 19:36:36 -0500

    Now displaying the basket component.

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 
 import mushroomData from '../helpers/data/mushroomData';
 import Forest from '../components/Forest/Forest';
+import Basket from '../components/Basket/Basket';
 
 class App extends React.Component {
   state= {
@@ -12,17 +13,27 @@ class App extends React.Component {
 
   componentDidMount() {
     const mushrooms = mushroomData.getMushrooms();
-    this.setState({ mushrooms });
+    const basket = mushroomData.getBasket();
+    this.setState({ mushrooms, basket });
   }
 
   render() {
-    const { mushrooms } = this.state;
+    const { mushrooms, basket } = this.state;
 
     return (
       <div className="App">
-        <h2>Mushroom Picker</h2>
+        <h1 className="display-1">Mushroom Picker</h1>
         <button className="m-3 btn btn-dark">Pick Mushroom</button>
-        <Forest mushrooms={mushrooms} />
+        <div className="container-fluid mt-3">
+          <div className="row">
+            <h2 className="forest-header text-center m-auto display-4">Forest</h2>
+            <Forest mushrooms={mushrooms} />
+          </div>
+          <div className="row">
+            <h2 className="basket-header text-center m-auto display-4">Basket</h2>
+            <Basket mushrooms={basket} />
+          </div>
+        </div>
       </div>
     );
   }
